67a680f2cb6725928b6e710568e3fff5152c8dbe
Tiago Abreu <devtiagoabreu@gmail.com>
2021-03-29 16:22:53 -0300

    Adequando SELECT ao OBJETO

--- registroInventarioTecidos.sql
+++ registroInventarioTecidos.sql
@@ -5,13 +5,15 @@
 
 --EM ESTOQUE SINTETICO
 SELECT
-	CP.Produto AS Codigo,
-	Produtos.Descricao,
+	CP.Produto AS ProdutoCodigo,
+	Produtos.Descricao AS ProdutoDescricao,
 	CP.Situacao,
 	CP.Cor,
+	Car_Cores.Descricao AS CorDescricao,
 	CP.Desenho,
 	CP.Variante,
-	REPLACE(SUM(ISNULL(CP.Metros,0)),'.',',') AS METROS
+	SUM(ISNULL(CP.Metros,0)) AS Metros,
+	SUM(ISNULL(CP.Peso,0)) AS Peso
 FROM 
 	DBMicrodata.dbo.Cte_Peca CP 
 	LEFT JOIN DBMicrodata.dbo.CTE_Baixa CB ON CP.Empresa=CB.Empresa and CP.Situacao=CB.Situacao and CP.Nro_Rolo=CB.Nro_Rolo and CP.Nro_Peca=CB.Nro_Peca
@@ -47,21 +49,30 @@ ORDER BY
 
 --EM ESTOQUE / CUSTO SITUACAO
 SELECT
-	CP.Produto AS Codigo,
-	Produtos.Descricao,
-	REPLACE((CASE   
+	CP.Produto AS ProdutoCodigo,
+	Produtos.Descricao AS ProdutoDescricao,
+	CP.Situacao,
+	CP.Cor,
+	Car_Cores.Descricao AS CorDescricao,
+	CP.Desenho,
+	CP.Variante,
+	SUM(ISNULL(CP.Metros,0)) AS Metros,
+	SUM(ISNULL(CP.Peso,0)) AS Peso,
+	ISNULL((CASE   
 		WHEN CP.Situacao = '000' THEN Produtos_Tecidos.Custo_Cru
 		WHEN CP.Situacao = '001' THEN Produtos_Tecidos.Custo_Remessa
 		WHEN CP.Situacao = '002' THEN Produtos_Tecidos.Custo_Estampado
 		WHEN CP.Situacao = '009' THEN Produtos_Tecidos.Custo_Estampado
-		ELSE Produtos_Tecidos.Custo_Outros	
-	END),'.',',') AS Custo,
-	REPLACE(Produtos_Tecidos.Custo_Outros,'.',',') AS CUSTO_OUTROS,
-	CP.Situacao,
-	CP.Cor,
-	CP.Desenho,
-	CP.Variante,
-	REPLACE(SUM(ISNULL(CP.Metros,0)),'.',',') AS METROS
+		ELSE '0'	
+	END),0) AS CustoMetro,
+	ISNULL(Produtos_Tecidos.Custo_Outros, 0) AS CustoMetroOutros,
+	ISNULL((CASE   
+		WHEN CP.Situacao = '000' THEN Produtos_Tecidos.Custo_Cru
+		WHEN CP.Situacao = '001' THEN Produtos_Tecidos.Custo_Remessa
+		WHEN CP.Situacao = '002' THEN Produtos_Tecidos.Custo_Estampado
+		WHEN CP.Situacao = '009' THEN Produtos_Tecidos.Custo_Estampado
+		ELSE '0'	
+	END),0) * SUM(ISNULL(CP.Metros,0)) AS CustoTotal
 FROM 
 	DBMicrodata.dbo.Cte_Peca CP 
 	LEFT JOIN DBMicrodata.dbo.CTE_Baixa CB ON CP.Empresa=CB.Empresa and CP.Situacao=CB.Situacao and CP.Nro_Rolo=CB.Nro_Rolo and CP.Nro_Peca=CB.Nro_Peca
@@ -73,10 +84,9 @@ WHERE
 	CP.Situacao like '%' AND 
 	CP.Nro_Rolo like '%' AND 
 	CP.Nro_Peca like '%' AND 
-	CP.Produto like '002518' AND 
+	CP.Produto like '%' AND 
 	Cor like '%' AND 
 	Desenho like '%' AND 
-	--Categoria Like '%01%' AND 
 	Categoria_Tinto Like '%01%' AND 
 	IsNull(Variante, '') Like '%' AND
 	CB.Data_Saida IS NULL
@@ -88,23 +98,30 @@ GROUP BY
 	Car_Cores.Descricao,
 	CP.Desenho,
 	CP.Variante,
-	CP.Categoria
+	CP.Categoria,
+	Produtos_Tecidos.Custo_Cru,
+	Produtos_Tecidos.Custo_Remessa,
+	Produtos_Tecidos.Custo_Estampado,
+	Produtos_Tecidos.Custo_Estampado,
+	Produtos_Tecidos.Custo_Outros
 ORDER BY
 	CP.Produto,
 	CP.Situacao,
 	CP.Cor,
 	CP.Desenho,
 	CP.Variante
-
+	
 --BAIXA POR INVENTARIO	
 SELECT
-	CP.Produto AS Codigo,
-	Produtos.Descricao,
+	CP.Produto AS ProdutoCodigo,
+	Produtos.Descricao AS ProdutoDescricao,
 	CP.Situacao,
 	CP.Cor,
+	Car_Cores.Descricao AS CorDescricao,
 	CP.Desenho,
 	CP.Variante,
-	REPLACE(SUM(ISNULL(CP.Metros,0)),'.',',') AS METROS,
+	SUM(ISNULL(CP.Metros,0)) AS Metros,
+	SUM(ISNULL(CP.Peso,0)) AS Peso,
 	CB.Observacao ,
 	CB.Corte_Altera,
 	CB.Data_Saida 
